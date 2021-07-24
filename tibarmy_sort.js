9a0407a989c748b96a8033c46d7e2e1d19d41ea8
ThanGrove <than@virginia.edu>
2021-07-24 11:13:58 -0400

    removing console code from js

--- tibarmy_sort.js
+++ tibarmy_sort.js
@@ -5,10 +5,8 @@
     attach: function (context, settings) {
       // Resubmit form upon sort order change
       $('select#edit-sort-order').change(function(e) {
-        console.log(e);
         var selectEl = $(e.target);
         var srchform = selectEl.parents('#views-exposed-form-search-page').eq(0);
-        console.log("submitting: ", srchform);
         srchform.submit();
       });
     }
